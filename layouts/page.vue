66c5bc1d92f1abdbf081000b0c63be84e5b8f7a8
oeyoews <jyao4783@gmail.com>
2023-12-09 21:14:44 +0800

    add types folder

---
+++ layouts/page.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-white text-black dark:bg-black dark:text-white min-h-screen">
+    <slot />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,12 @@
 <script setup lang="ts">
+import type { User } from '~/types/user'
 
 onMounted(() => {
   console.log('Nuxt startup !!!')
 })
 
+// @ts-ignore
 const { data }: { data: User[] } = await useFetch('https://jsonplaceholder.typicode.com/users')
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: Address;
-  phone: string;
-  website: string;
-  company: Company;
-}
-
-interface Address {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo: Geo;
-}
-
-interface Geo {
-  lat: string;
-  lng: string;
-}
-
-interface Company {
-  name: string;
-  catchPhrase: string;
-  bs: string;
-}
-
 
 useHead({
   title: 'Nuxt HomePage',
